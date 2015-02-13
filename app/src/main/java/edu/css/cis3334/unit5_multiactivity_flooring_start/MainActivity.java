7ff45963b49720932d24f80bdc2ac379a2691c79
smueggenberg <smueggenberg@css.edu>
2015-02-13 14:10:55 -0600

    Added results activity using button listener

--- app/src/main/java/edu/css/cis3334/unit5_multiactivity_flooring_start/MainActivity.java
+++ app/src/main/java/edu/css/cis3334/unit5_multiactivity_flooring_start/MainActivity.java
@@ -1,39 +1,42 @@
 package edu.css.cis3334.unit5_multiactivity_flooring_start;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    Button calculate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        calculate = (Button) findViewById(R.id.btnShowResults);
+
+        calculate.setOnClickListener(new Button.OnClickListener(){
+            public void onClick(View view) {
+//                setupActivity();
+                Intent i = new Intent(MainActivity.this, ResultsActivity.class);
+
+                startActivity(i);
+            }
+        });
     }
 
+//    public void setupActivity() {
+//        Intent i = new Intent(this, ResultsActivity.class);
+//        startActivity(i);
+//    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
+//    public void onClick(){
+//        Intent i = new Intent(this, ResultsActivity.class);
+//        startActivity(i);
+//    }
 }
